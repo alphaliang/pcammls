646758c1e217f94631c73c4e088e41e6ddc78a99
alpha.liang <alpha.liang@gmail.com>
2019-08-21 01:25:48 +0800

    java

--- java/test.java
+++ java/test.java
@@ -1,7 +1,8 @@
-//import com.percipio.*;
+import com.percipio.pcammls.*;
 
 public class test{
 	public static void main(String[] args){
 		System.out.println("hello");
+		System.loadLibrary("pcammls");
 	}
 }
