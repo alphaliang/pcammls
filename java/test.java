7cb47da7d08ddd6d6593b3cf0d3378452ffce541
alpha.liang <alpha.liang@gmail.com>
2019-08-22 01:53:55 +0800

    build test

--- java/test.java
+++ java/test.java
@@ -4,5 +4,10 @@ public class test{
 	public static void main(String[] args){
 		System.out.println("hello");
 		System.loadLibrary("pcammls");
+        pcammls.TYInitLib();
+        TY_VERSION_INFO ver = new TY_VERSION_INFO();
+        pcammls.TYLibVersion(ver);
+        System.out.printf("%d.%d.%d\n",ver.getMajor(),ver.getMinor(),ver.getPatch());
+        pcammls.TYDeinitLib();
 	}
 }
